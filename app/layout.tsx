f5829fb81f11fee0f9a204f731728761dbc80d7d
Carl Wicker <carlwicker@gmail.com>
2023-01-24 22:02:42 +0000

    nextjs, tailwind, firebase setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,37 @@
-import './globals.css'
+"use client";
+
+import { initializeApp } from "firebase/app";
+import { useEffect } from "react";
+import "./globals.css";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  useEffect(() => {
+    console.log(process.env);
+    console.log(process.env.NEXT_PUBLIC_APPID);
+
+    return () => {};
+  }, []);
+
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
