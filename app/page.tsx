81177a5888d1531f8963f81347da2c30bfad4b32
Carl Wicker <carlwicker@gmail.com>
2023-02-04 14:01:30 +0000

    added footer, experimenting with framer scroll

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,19 @@
 import Footer from "@/components/footer/Footer";
 import LatestReleases from "@/components/latestReleases/latestReleases";
 import TopBar from "@/components/topbar/TopBar";
-import { motion, useScroll, useSpring, useMotionValue } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 export default function Home() {
-  const { scrollYProgress } = useScroll();
+  const { scrollYProgress } = useScroll({});
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 1000,
     damping: 100,
-  }) as any; // only way to get .current to work in linter
+  });
 
   return (
     <>
       <motion.div
-        style={{ scaleX: scaleX, rotateX: scaleX.current * 5000 }}
+        style={{ scaleX: scaleX }}
         className="bg-red-500 fixed top-0 left-0 right-0 h-[4px] origin-left -z-10"
       />
       <div className="container mx-auto text-left">
